3c7690a04036373a46d74f3f4bfe1d153a061043
phattarakorncagla <phattarakorn@cagla.jp>
2025-02-04 18:06:13 +0900

    Update

--- note-app/src/NoteForm.tsx
+++ note-app/src/NoteForm.tsx
@@ -1,18 +1,21 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable"
 import { NoteData, Tag } from "./App";
 import { v4 as uuidV4 } from "uuid"
 
 type NoteFormProps = {
     onSubmit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void;
+    availableTags: Tag[];
 }
 
-export function NoteForm({ onSubmit }: NoteFormProps) {
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [SelectedTags, setSelectedTags] = useState<Tag[]>([])
+    const navigate = useNavigate()
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault()
@@ -22,6 +25,8 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
             markdown: markdownRef.current!.value,
             tags:[]
         })
+
+        navigate("..")
     }
 
     return (
@@ -44,8 +49,11 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
                             setSelectedTags(prev => [...prev, newTag])
                         }}
                         value={SelectedTags.map(tag => {
-                            return { label: tag.label, value: tag.id }ß
-                        })} 
+                            return { label: tag.label, value: tag.id }
+                        })}
+                        options={availableTags.map(tag => {
+                            return { label: tag.label, value: tag.id }
+                        })}
                         onChange={tags => {
                             setSelectedTags(tags.map(tag => {
                                 return { label: tag.label, id: tag.label }
---
+++ note-app/src/NoteList.tsx
@@ -0,0 +1,55 @@
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select"
+
+export function NoteList( {availableTags} ) {
+    // 39.20
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Notes</h1>
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                value={SelectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.label };
+                    })
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+}
